f2dafa8d670d7fc78c66f33fbdd76208b2aba3fd
Lokesh Rangineni <lokeshforjava@gmail.com>
2023-05-05 01:28:10 -0400

    1) Added the code to communicate with the rest API end points of keycloak. Added keycloak admin client library. 2) Added the keycloak realm config export for future reference.

--- src/main/java/com/redhat/openid/client/keycloack/openid/client/controller/UserRestService.java
+++ src/main/java/com/redhat/openid/client/keycloack/openid/client/controller/UserRestService.java
@@ -1,15 +1,27 @@
 package com.redhat.openid.client.keycloack.openid.client.controller;
 
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
-public class RestResource {
+public class UserRestService {
+
+    @Autowired
+    private Keycloak keycloak;
+
+    @Value("${keycloak.realm}")
+    private String realm;
 
     @GetMapping("/user-info")
     public Jwt getOidcUserPrincipal(
@@ -18,8 +30,8 @@ public class RestResource {
     }
 
     @GetMapping("/users")
-    public Jwt findUsers(
+    public List<UserRepresentation> findUsers(
             @AuthenticationPrincipal Jwt principal) {
-        return principal;
+        return keycloak.realm(realm).users().list();
     }
 }
